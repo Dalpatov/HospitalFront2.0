d02235548c19a6abf37de94509c2f71c800c1725
Dalpatov <dalpatov.exceedteam@gmail.com>
2021-04-28 14:22:04 +0300

    Create Page Autorization(task3)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
-// import AutorizationPage from './Autorization/AutorizationPage';
+import AutorizationPage from './Components/Autorization/AutorizationPage';
 import RegistrationPage from './Components/Registration/RegistrationPage';
 import MainTab from './Components/MainTab/MainTab';
 import './App.css';
@@ -11,9 +11,9 @@ function App() {
       <Switch>
         <Route path="/maintab" component={MainTab} />
         <Route path="/registration" component={RegistrationPage} />
-        {/* <Route path='/autorization' component={AutorizationPage}/>  */}
+        <Route path='/autorization' component={AutorizationPage}/> 
       </Switch>
-      <Redirect from="/" to="/registration" />
+      <Redirect from="/" to="/autorization" />
     </div>
   );
 }
